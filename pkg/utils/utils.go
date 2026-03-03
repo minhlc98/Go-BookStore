26fe40c96056f12dba1596c874ce5c913caffd4c
Cam Minh <minhlc98@gmail.com>
2026-03-03 17:14:54 +0700

    chore: remove unnecessary log

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,13 +2,11 @@ package utils
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 )
 
 func ParseBody(r *http.Request, x interface{}) {
-	fmt.Println(r.Body)
 	if body, err := io.ReadAll(r.Body); err == nil {
 		if err := json.Unmarshal([]byte(body), x); err != nil {
 			return
