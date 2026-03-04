026c11abfdfdf3ad2aa2c2e42f90f41ad26023f9
Cam Minh <minhlc98@gmail.com>
2026-03-04 13:21:57 +0700

    fix: correct recover condition

--- pkg/middleware/recovery.middleware.go
+++ pkg/middleware/recovery.middleware.go
@@ -8,7 +8,7 @@ import (
 func Recovery(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func () {
-			if rec := recover(); r != nil {
+			if rec := recover(); rec != nil {
 				log.Printf("[PANIC] %v | %s %s\n", rec, r.Method, r.URL.Path)
 				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			}
